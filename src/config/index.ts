7365c393065e4ad2386ebd8e565101e9c94394db
ZBM Zubayer <zbmzubayer@gmail.com>
2023-10-11 01:00:56 +0600

    Vercel env for vite reconfigured 2

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const envConfig = {
-  API_BASE_URL: (process.env.VITE_API_BASE_URL as string) || 'http://localhost:5000/api/v1',
+  API_BASE_URL: (import.meta.env.VITE_API_BASE_URL as string) || 'http://localhost:5000/api/v1',
 };
 
 export default envConfig;
