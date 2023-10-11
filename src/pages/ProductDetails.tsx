262868b87071554f1e0d0d0bb46617ef5057c57c
ZBM Zubayer <zbmzubayer@gmail.com>
2023-10-12 01:57:51 +0600

    Vercel final deploy

--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,36 +1,59 @@
+import LoadingScreen from '@/components/LoadingScreen';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { IProduct } from '@/interfaces/product.interface';
+import { cn } from '@/lib/utils';
+import { addToCart } from '@/redux/features/cart/cartSlice';
 import { useGetProductByIdQuery } from '@/redux/features/product/productApi';
+import { useAppDispatch } from '@/redux/hook';
 import { Link, useParams } from 'react-router-dom';
 
 export default function ProductDetails() {
   const { id } = useParams();
   const { data, isLoading } = useGetProductByIdQuery(id!);
   const product: IProduct = data?.data;
-  console.log(product?.subCategory);
 
-  if (isLoading) return <div className="text-center">Loading...</div>;
+  const dispatch = useAppDispatch();
+
+  if (isLoading) return <LoadingScreen />;
   return (
-    <div className="space-y-8">
-      <div className="grid grid-cols-3 gap-10 bg-gray-400">
-        <div className="flex justify-center items-center p-1 bg-white rounded-md border">
-          <img src={product?.image} alt={product?.name} className="h-[350px] aspect-square object-contain" />
+    <div className="space-y-8 mt-10">
+      <div className="flex justify-center gap-5">
+        <div className="flex-shrink-0 p-2 rounded-md border">
+          <img src={product?.image} alt={product?.name} className="h-[350px] w-[350px] object-contain" />
         </div>
-        <div className="col-span-2">
-          <h1 className="text-2xl font-medium">{product?.name}</h1>
-          <Link to={`/store/${product?.seller?.id}`} className="text-cyan-600 hover:underline hover:underline-offset-2">
-            Visit the {product?.seller?.companyName} Store
-          </Link>
+        <div className="">
           <div>
-            <p>Status: {product?.status}</p>
-            <p>
-              <span>Availability: </span>
-              <span className={`${product?.stock > 0 ? 'text-green-600' : 'text-red-600'} font-medium`}>
-                {product?.stock > 0 ? 'In stock' : 'Out of stock'}
-              </span>
-            </p>
-            <p>Sold count: {product?.soldCount}</p>
-            <p>Rating: 4</p>
-            <p className="text-xl font-bold tracking-wider">${product?.price}</p>
+            <h1 className="text-2xl font-medium">{product?.name}</h1>
+            <Link
+              to={`/store/${product?.seller?.id}`}
+              className="text-cyan-600 hover:underline hover:underline-offset-2"
+            >
+              Visit the {product?.seller?.companyName} Store
+            </Link>
+            <div className="mt-3">
+              <p>Status: {product?.status}</p>
+              <p>
+                <span>Availability: </span>
+                <span className={`${product?.stock > 0 ? 'text-green-600' : 'text-red-600'} font-medium`}>
+                  {product?.stock > 0 ? 'In stock' : 'Out of stock'}
+                </span>
+              </p>
+              <p>Sold count: {product?.soldCount}</p>
+              <p>Warranty: {product?.warranty}</p>
+              <p>Rating: 4</p>
+              <p className="mt-3 text-xl font-bold tracking-wider">${product?.price}</p>
+            </div>
+          </div>
+          <div className="mt-10 space-x-5">
+            <Button className="text-lg group w-[200px]" onClick={() => dispatch(addToCart(product))}>
+              Add to cart
+            </Button>
+            <Link
+              to="/customer/checkout"
+              className={cn(buttonVariants(), 'text-lg w-[200px] bg-cyan-500 hover:bg-cyan-600')}
+            >
+              Buy Now
+            </Link>
           </div>
         </div>
       </div>
